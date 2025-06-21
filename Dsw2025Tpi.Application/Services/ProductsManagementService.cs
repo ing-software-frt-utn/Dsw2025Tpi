803078feb2554ffb08737913819ffc4b47ac09cf
joel <angeljoeltoledo@gmail.com>
2025-06-21 16:20:20 -0300

    AVANCES.APPLICATION

---
+++ Dsw2025Tpi.Application/Services/ProductsManagementService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dsw2025Tpi.Application.Exceptions;
+using Dsw2025Tpi.Application.Dtos;
+using Dsw2025Tpi.Domain.Interfaces;
+
+
+
+namespace Dsw2025Tpi.Application.Services;
+
+public class ProductsManagementService
+{
+    private readonly IRepository _repository;
+
+    public ProductsManagementService(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Product?> GetProductById(Guid id)
+    {
+        return await _repository.GetById<Product>(id);
+    }
+
+    //se modifico respecto a la original
+    public async Task<List<Product>?> GetProducts()
+    {
+        var products = await _repository.GetAll<Product>();
+        return products?.ToList() ?? new List<Product>();
+
+    }
+
+    public async Task<ProductModel.Response> AddProduct(ProductModel.Request request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Sku) ||
+            string.IsNullOrWhiteSpace(request.Name) ||
+            request.Price < 0)
+        {
+            throw new ArgumentException("Valores para el producto no válidos");
+        }
+
+        var exist = await _repository.First<Product>(p => p.Sku == request.Sku);
+        if (exist != null) throw new DuplicatedEntityException($"Ya existe un producto con el Sku {request.Sku}");
+
+        var product = new Product(request.Sku, request.Name, request.Price, request.);
+        await _repository.Add(product);
+        return new ProductModel.Response(product.Id);
+    }
+}
