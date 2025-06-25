309b77733fddd1f7a29fb9d8307601d61d246c82
Salitas01 <salasfacundo15@gmail.com>
2025-06-25 18:53:40 -0300

    clases del dominio instanciadas

---
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities;
+
+internal class Customer : EntityBase
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public List<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ Dsw2025Tpi.Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities;
+
+internal class Order : EntityBase
+{
+    public DateTime date { get; set; }
+    public string shippingAdress { get; set; }
+    public string billingAdress { get; set; }
+    public string notes { get; set; }
+    public decimal totalAmount { get; set; }
+
+    public List<Product> products { get; set; } = new List<Product>();
+    public List<Order> orders { get; set; } = new List<Order>();
+
+}
