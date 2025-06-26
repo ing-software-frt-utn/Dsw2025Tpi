fba2306db4cb0c311aa8691728b2cfeb23b5ce86
Salitas01 <salasfacundo15@gmail.com>
2025-06-26 20:39:44 -0300

    Models instanciados

---
+++ Dsw2025Tpi.Application/Dtos/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Dtos;
+
+internal record ProductModel
+{
+    public record Request(string sku, string name, decimal currentUnitPrice, int stockQuantity, string? internalCode, string? description);
+
+    public record Update(string sku, string name, decimal currentUnitPrice, int stockQuantity, string? internalCode, string? description);
+
+    public record Response(Guid Id, string sku, string name, decimal currentUnitPrice, int stockQuantity, bool isActive);
+}
---
+++ Dsw2025Tpi.Application/Services/ProductManagementService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Tpi.Application.Dtos.Products;
+
+namespace Dsw2025Tpi.Application.Services;
+
+public class ProductManagementService
+{
+    
+}
