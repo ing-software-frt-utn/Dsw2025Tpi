4cbd32cc84d42219a04befdb195b2b3ca20bb75b
Juan Lopez Asis <juan.lopezasis27@hotmail.com>
2025-06-26 02:59:23 -0300

    Se agrega ProductsController

---
+++ Dsw2025Tpi.Api/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2025Tpi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
