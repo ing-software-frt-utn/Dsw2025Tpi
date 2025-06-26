beec826b78ba6549d8d18f338d7eb49c861ee829
Salitas01 <salasfacundo15@gmail.com>
2025-06-26 20:54:15 -0300

    Models-instanciados

--- Dsw2025Tpi.Domain/Entities/Customer.cs
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -4,14 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dsw2025Tpi.Domain.Entities;
-
-internal class Customer : EntityBase
+namespace Dsw2025Tpi.Domain.Entities
 {
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Address { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public List<Order> Orders { get; set; } = new List<Order>();
+    internal class Customer
+    {
+    }
 }
