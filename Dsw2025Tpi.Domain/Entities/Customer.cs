a366d85b79eebf83d21889de37dcfb9e9a99f410
Celina-zts <celinazatososa@gmail.com>
2025-06-27 13:32:56 -0300

    Se crearon las entidades Customer y Order.

---
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class Customer : EntityBase
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Customer(string name, string email, string phoneNumber)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+    }
+}
---
+++ Dsw2025Tpi.Domain/Entities/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class Order : EntityBase
+    {
+        public OrderStatus Status { get; set; } 
+        public DateTime CreateDate { get; set; }
+        public string ShippingAddress { get; set; }
+        public string BillingAddress { get; set; }
+        public string Notes { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public Order(DateTime createDate, string shippingAddress, string billingAddress, string notes, decimal totalAmount)
+        {
+            Id = Guid.NewGuid();
+            Status = OrderStatus.Pending; // Default status
+            CreateDate = createDate;
+            ShippingAddress = shippingAddress;
+            BillingAddress = billingAddress;
+            Notes = notes;
+            TotalAmount = totalAmount;
+        }
+    }
+}
