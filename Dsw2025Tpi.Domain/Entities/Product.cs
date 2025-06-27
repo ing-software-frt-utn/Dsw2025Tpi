c94f0ff4ea3bc7f691c7689a6147ac05eacc0875
DylanSDev <dylansdiaz.dev@gmail.com>
2025-06-27 13:04:29 -0300

    Se agregó la entidad producto.

---
+++ Dsw2025Tpi.Domain/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public string Sku { get; set; }
+        public string InternalCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal CurrentUnitPrice { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        public Product()
+        { }
+
+        public Product(string sku, string internalCode, string name, string description, decimal currentUnitPrice, int stockQuantity)
+        {
+            Id = Guid.NewGuid();
+            Sku = sku;
+            InternalCode = internalCode;
+            Name = name;
+            Description = description;
+            CurrentUnitPrice = currentUnitPrice;
+            StockQuantity = stockQuantity;
+            IsActive = true;
+        }
+    }
+}
